697a72aed394a98729134faaa2941cc354d068d2
ARElton <ameliareneeelton@gmail.com>
2020-11-30 17:19:14 -0500

    build out models and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,35 @@
 class PostsController < ApplicationController
+    before_action: 
+
+    def index
+        @posts = Post.all
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        post = Post.create(strong_params)
+        if post.valid?
+            redirect_to post_path(post)
+        else
+            flash[:user_errors] = post.errors.full_messages
+            redirect_to new_post_path
+        end
+    end
+
+    def delete
+    end
+
+    private
+
+    def strong_params
+        params.require(:post).permit(:user_id, :city_id, :title, :link)
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+
+    def new
+        @user = User.new 
+    end
+
+    def create
+        user = User.create(user_params)
+        if user.valid?
+            redirect_to user_path(user)
+        else 
+            flash[:user_errors] = user.errors.full_messages 
+            redirect_to new_user_path 
+        end
+    end
+
+    def index 
+        @users = User.all 
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def edit 
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        @user.update(user_params)
+
+        redirect_to user_path(@user)
+    end
+
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy 
+
+        redirect_to users_path 
+    end 
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :age, :username, :password)
+    end
 end
