14446e60ba9f8ba0a269dc6dcf5a84bfe3023027
ARElton <ameliareneeelton@gmail.com>
2020-12-01 17:27:30 -0500

    update comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
 
-    def new
-        @comment = Comment.new
-    end
+    # def new
+    #     @comment = Comment.new
+    # end
 
     def create
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(params[:comment]).permit(:user_id, :post_id, :text)
+        @comment = @post.comments.create(comment_params)
         redirect_to post_path(@post)
     end
 
@@ -15,6 +15,12 @@ class CommentsController < ApplicationController
         @comment = @post.comments.find(params[:id])
         @comment.destroy 
         redirect_to post_path(@post)
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:user_id, :post_id, :text)
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :favorites
   resources :cities
+  root 'cities#index'
   resources :posts do 
     resources :comments 
   end
+  # resources :comments 
+  # get '/posts/:id/comments/:id', to: 'comments#new', as: 'new_comment'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
